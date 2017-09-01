d2a705b42e675292ee137f47444a75cc7f86aca2
Asher Van Brunt <asher@okbreathe.com>
2017-08-31 19:15:25 -0700

    Specify column type in migration

--- db/migrate/20140301141327_add_stripe_customer_id_to_spree_users.rb
+++ db/migrate/20140301141327_add_stripe_customer_id_to_spree_users.rb
@@ -1,6 +1,6 @@
 class AddStripeCustomerIdToSpreeUsers < ActiveRecord::Migration
   def change
     add_column Spree.user_class.to_s.pluralize.downcase.to_sym, :stripe_customer_id, :string
-    remove_column :spree_subscriptions, :card_customer_token
+    remove_column :spree_subscriptions, :card_customer_token, :string
   end
 end
